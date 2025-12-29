446673c0be494d1f6feabe139ee6394eabc3a602
chirag-bgh <chirag2bsingh@gmail.com>
2025-12-29 16:39:41 +0530

    fix: extend flashblocks

--- crates/flashblocks/src/pending.rs
+++ crates/flashblocks/src/pending.rs
@@ -52,8 +52,11 @@ impl PendingBlocksBuilder {
     }
 
     #[inline]
-    pub(crate) fn with_flashblocks(&mut self, flashblocks: Vec<Flashblock>) -> &Self {
-        self.flashblocks = flashblocks;
+    pub(crate) fn with_flashblocks(
+        &mut self,
+        flashblocks: impl IntoIterator<Item = Flashblock>,
+    ) -> &Self {
+        self.flashblocks.extend(flashblocks);
         self
     }
 
